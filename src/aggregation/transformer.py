b8191d1f26bfdb8f44628b28f54be41d924fccce
iejMac <kilianmaciej6@gmail.com>
2022-07-28 14:18:34 +0000

    much better

--- src/aggregation/transformer.py
+++ src/aggregation/transformer.py
@@ -22,9 +22,9 @@ class PositionalEncoding(nn.Module):
         super().__init__()
         position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        pe = torch.zeros(max_len, 1, d_model)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        pe = torch.zeros(1, max_len, d_model)
+        pe[0, :, 0::2] = torch.sin(position * div_term)
+        pe[0, :, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
