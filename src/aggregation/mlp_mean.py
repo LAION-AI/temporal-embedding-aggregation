a08aa12f1d2922d7c2b1dff1a98fc95a85f4bb6a
iejMac <kilianmaciej6@gmail.com>
2022-10-24 22:47:04 +0000

    aggregation: baseline method MLPMean

---
+++ src/aggregation/mlp_mean.py
@@ -0,0 +1,18 @@
+"""
+Transformer for encoding sequences of frame embeddings
+"""
+import math
+import torch
+import torch.nn.functional as F
+
+class MLPMean(nn.Module):
+    def __init__(self, dim, proj_dim):
+        super().__init__()
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, proj_dim)
+        )
+    def forward(self, x):
+        x = self.mlp_head(x)
+        x = torch.mean(x, 1)
+        return x
