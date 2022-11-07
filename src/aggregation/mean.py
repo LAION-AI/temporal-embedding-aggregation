95bbc61ce32cbaeaf4876df9ce49f8b992743f18
danielmend <31258255+danielmend@users.noreply.github.com>
2022-11-06 20:41:14 -0800

    normalize mean aggregator output

--- src/aggregation/mean.py
+++ src/aggregation/mean.py
@@ -5,9 +5,9 @@ video_embedding = mean(frame_embeddings)
 """
 
 from torch import nn
-
+import torch.nn.functional as F
 class Mean(nn.Module):
   def __init__(self):
     super().__init__()
   def forward(self, x):
-    return x.mean(axis=-2) # assumes shape always ends with (..., n_frames, embed_dim)
+    return F.normalize(x.mean(axis=-2), dim=-1) # assumes shape always ends with (..., n_frames, embed_dim)
