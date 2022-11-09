e3dc86ece6eac17d16e9916ab46d02afbd33baa0
danielmend <31258255+danielmend@users.noreply.github.com>
2022-11-09 15:09:49 -0800

    aggregator_wrapper: add default values for pre and post norm

--- src/aggregation/aggregator_wrapper.py
+++ src/aggregation/aggregator_wrapper.py
@@ -12,7 +12,7 @@ class VideoCLIP(nn.Module):
         self.aggregator = aggregator
         self.logit_scale = torch.nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
 
-    def forward(self, x, prenorm, postnorm):
+    def forward(self, x, prenorm=True, postnorm=True):
         x = F.normalize(x, dim=-1) if prenorm else x
         x = self.aggregator(x)
         x = F.normalize(x, dim=-1) if postnorm else x
