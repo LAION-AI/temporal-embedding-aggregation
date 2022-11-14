8620aab73bce08f1c36b51675566ddebf29daace
danielmend <31258255+danielmend@users.noreply.github.com>
2022-11-13 17:05:50 -0800

    evaluation/retrieval.py: undo the eval changes

--- src/evaluation/retrieval.py
+++ src/evaluation/retrieval.py
@@ -1,7 +1,7 @@
 import open_clip
 import numpy as np
 import torch
-
+from einops import rearrange
 
 def retrieval_evaluation(model_video, model_text, data, multicaption=False):
     if type(data) == dict:
@@ -10,9 +10,8 @@ def retrieval_evaluation(model_video, model_text, data, multicaption=False):
         dataloader = data
     device = "cuda" if torch.cuda.is_available() else "cpu"
     all_video_features, all_text_features = [], []
-    ground_truth = []
-    samp = 0
-
+    max_txt_len = 1
+    
     with torch.no_grad():
         for i, batch in enumerate(dataloader):
             embeddings = batch["embeddings"]
@@ -20,61 +19,90 @@ def retrieval_evaluation(model_video, model_text, data, multicaption=False):
             # TODO: does this require batch_size = 1 ??
             for cap in batch["text"]:
                 if multicaption:
-                    for c in cap.split(";"): # multiple captions separated by ;
+                    caps_list = cap.split(';')
+                    max_txt_len = max(max_txt_len, len(caps_list))
+            
+                    for c in caps_list: # multiple captions separated by ;
                         toks.append(open_clip.tokenize(c))
-                        ground_truth.append(samp)
                 else:
                     toks.append(open_clip.tokenize(cap))
-                    ground_truth.append(samp)
-                samp += 1
+
             toks = torch.cat(toks)
             embeddings = embeddings.to(device, non_blocking=True)
             toks = toks.to(device, non_blocking=True)
 
             video_embeddings = model_video(embeddings)
-            text_embeddings = model_text(toks).float()
+            text_embeddings = model_text(toks)
 
             all_video_features.append(video_embeddings.cpu())
             all_text_features.append(text_embeddings.cpu())
 
+        dim_model = all_video_features[0].shape[-1]
+
+        text_features = torch.stack(
+            zero_pad_text_features(all_text_features, max_txt_len, dim_model=dim_model)
+        ) if multicaption else torch.cat(all_text_features).unsqueeze(1)
+        video_features = torch.cat(all_video_features)
+        
         val_metrics = get_metrics(
-            video_features=torch.cat(all_video_features),
-            text_features=torch.cat(all_text_features),
-            ground_truth=ground_truth,
+            video_features=video_features,
+            text_features=text_features,
             logit_scale=100.0,
         )
     return val_metrics
 
+def zero_pad_text_features(text_features, max_txt_len, dim_model=512):
+    out = []
+    for mat in text_features:
+        padded = torch.zeros(size=(max_txt_len, dim_model))
+        padded[0:len(mat), :] = mat
 
-def get_metrics(video_features, text_features, ground_truth, logit_scale):
+        out.append(padded)
+
+    return out
+    
+
+def get_metrics(video_features, text_features, logit_scale):
+    ''' 
+    Assumptions for this eval:
+    - len(text_features) % len(video_features) == 0, i.e. there's a constant number 
+    of captions per video (that constant can be 1).
+    - video_features and text_features are arranged such that if we repeat
+    each element of video_features N times along axis=0, with N being the nunber
+    of captions per video, video_features[i] has ground truth label 
+    text_features[i] for 0 <= i <= len(text_features), i.e. each index of
+    video_features and text_features gives you a matching video-text pair 
+    - both model_video and model_text spit out NORMALIZED embeddings
+    Einops notation:
+    n = num captions per video
+    v = num videos
+    t = num captions
+    '''
     metrics = {}
-
     video_features = video_features.float()
-    logits_per_video = (logit_scale * video_features @ text_features.t()).detach().cpu()
-    logits_per_text = logits_per_video.t().detach().cpu()
-
-
-    # TODO: let's to text2video correctly and then figure out how to do video2text
-    # maybe video2text is average logits over multiple captions
-    '''
-    # Average out over every 20 texts
-    avg_per_20 = torch.zeros((video_features.shape[0], video_features.shape[0]))
-    for i in range(video_features.shape[0]):
-        avg_per_20[i, :] = torch.mean(logits_per_video[:, i*20:(i+1)*20], axis=-1)
-    logits_per_video = avg_per_20
-    '''
-
-    # logits = {"video_to_text": logits_per_video, "text_to_video": logits_per_text}
-    logits = {"text_to_video": logits_per_text}
-    ground_truth = torch.tensor(ground_truth).view(-1, 1)
+    text_features = text_features.float()
+    
+    logits_per_text = (logit_scale * text_features @ video_features.T)
+    logits_per_text = rearrange(logits_per_text, 'v n t -> n v t')
+    
+    min_rank, _ = torch.max(logits_per_text, dim=0)
+    logits_per_video = min_rank.t().unsqueeze(0)
+    
+    logits = {'video_to_text': logits_per_video, 'text_to_video': logits_per_text}
 
     for name, logit in logits.items():
-        ranking = torch.argsort(logit, descending=True)
-        preds = torch.where(ranking == ground_truth)[1]
-        preds = preds.detach().cpu().numpy()
-        metrics[f"{name}_mean_rank"] = preds.mean() + 1
-        metrics[f"{name}_median_rank"] = np.floor(np.median(preds)) + 1
+        argsorted = torch.argsort(logit, dim=-1, descending=True)
+        ranks_on_diagonals = torch.argsort(argsorted, dim=-1, descending=False)
+        
+        rankings = torch.diagonal(ranks_on_diagonals, dim1=-1, dim2=-2).flatten().float()
+        
+        diagonal_logits = torch.diagonal(logit, dim1=-1, dim2=-2).flatten().float()
+        mask = ~(diagonal_logits == 0) # make sure we take out zero padded logits
+        rankings = rankings[mask]
+            
+        metrics[f"{name}_mean_rank"] = (rankings.mean() + 1).item()
+        metrics[f"{name}_median_rank"] = (torch.floor(torch.median(rankings)) + 1).item()
         for k in [1, 5, 10]:
-            metrics[f"{name}_R@{k}"] = np.mean(preds < k)
+            metrics[f'{name}_R@{k}'] = torch.mean((rankings < k).float()).item()
 
     return metrics
