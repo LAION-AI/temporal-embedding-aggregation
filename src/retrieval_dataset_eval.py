053639f72d8f1f4c9d9fc857ae6de04fb925c1d4
danielmend <31258255+danielmend@users.noreply.github.com>
2022-11-17 14:04:59 -0800

    update device for clip models

--- src/retrieval_dataset_eval.py
+++ src/retrieval_dataset_eval.py
@@ -1,4 +1,5 @@
 import open_clip
+import torch 
 
 from clip_video_encode.dataset import EmbeddingWebDatasetReader
 from aggregation.mean import Mean
@@ -7,6 +8,10 @@ from evaluation.retrieval import retrieval_evaluation
 
 oc_h14, _, preprocess = open_clip.create_model_and_transforms("ViT-H-14", pretrained="laion2b_s32b_b79k")
 oai_b32, _, preprocess = open_clip.create_model_and_transforms("ViT-B-32", pretrained="openai")
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+oc_h14.to(device)
+oai_b32.to(device)
 
 def evaluate_datasets_and_ckpts(eval_data):
     for ckpt, tar, multicaption in eval_data:
