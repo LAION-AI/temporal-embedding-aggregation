b002b7450a4cc645848d209bebce798a9b98f308
danielmend <31258255+danielmend@users.noreply.github.com>
2022-11-17 13:09:03 -0800

    retrieval_dataset_eval: refactor script to one function

--- src/retrieval_dataset_eval.py
+++ src/retrieval_dataset_eval.py
@@ -10,10 +10,14 @@ from aggregation.mean import Mean
 from aggregation.aggregator_wrapper import VideoCLIP
 from evaluation.retrieval import retrieval_evaluation
 
-class RetrievalDatasetEvaluator:
-    def __init__(self, val_tars, aggregator, clip_model, multicaption=False):
-        self.val_reader = EmbeddingWebDatasetReader(
-            val_tars,
+oc_h14, _, preprocess = open_clip.create_model_and_transforms("ViT-H-14", pretrained="laion2b_s32b_b79k")
+oai_b32, _, preprocess = open_clip.create_model_and_transforms("ViT-B-32", pretrained="openai")
+
+def evaluate_datasets_and_ckpts(eval_data):
+    for ckpt, tar, multicaption in eval_data:
+        assert isinstance(ckpt, VideoCLIP)
+        val_reader = EmbeddingWebDatasetReader(
+            tar,
             standard_seq_len=200,
             batch_size=1,
             num_prepro_workers=6,
@@ -21,31 +25,17 @@ class RetrievalDatasetEvaluator:
             enable_text=False,
             enable_meta=True
         )
-        self.video_clip = VideoCLIP(aggregator, clip_model)
-        self.multicaption = multicaption
+        metrics = retrieval_evaluation(ckpt, val_reader, multicaption)
+        print(metrics)
 
-    def evaluate(self):
-        return retrieval_evaluation(self.video_clip, self.val_reader, self.multicaption)
-
-oc_h14, _, preprocess = open_clip.create_model_and_transforms("ViT-H-14", pretrained="laion2b_s32b_b79k")
-oai_b32, _, preprocess = open_clip.create_model_and_transforms("ViT-B-32", pretrained="openai")
-
-def evaluate_retrieval(aggregators, datasets, multicaption=True):
-    for aggregator in aggregators:
-        for tar, clip_model in datasets:
-            evaluator = RetrievalDatasetEvaluator(tar, aggregator, clip_model, multicaption)
-            metrics = evaluator.evaluate()
-            print(metrics)
+vc_oc_h14 = VideoCLIP(Mean(), oc_h14)
+vc_oai_b32 = VideoCLIP(Mean(), oai_b32)
 
 eval_data = [
-    ('pipe:aws s3 cp s3://s-laion/msr_vtt/clip_msr_vtt/oc_h14/test/000000000.tar -', oc_h14),
-    ('pipe:aws s3 cp s3://s-laion/msr_vtt/clip_msr_vtt/oai_b32/test/000000000.tar -', oai_b32),
-    ('pipe:aws s3 cp s3://s-laion/msvd/clip_msvd/oc_h14/test/000000000.tar -', oc_h14),
-    ('pipe:aws s3 cp s3://s-laion/msvd/clip_msvd/oai_b32/test/000000000.tar -', oai_b32),
+    (vc_oc_h14, 'pipe:aws s3 cp s3://s-laion/msr_vtt/clip_msr_vtt/oc_h14/test/000000000.tar -', True),
+    (vc_oai_b32, 'pipe:aws s3 cp s3://s-laion/msr_vtt/clip_msr_vtt/oai_b32/test/000000000.tar -', True),
+    (vc_oc_h14, 'pipe:aws s3 cp s3://s-laion/msvd/clip_msvd/oc_h14/test/000000000.tar -', True),
+    (vc_oai_b32, 'pipe:aws s3 cp s3://s-laion/msvd/clip_msvd/oai_b32/test/000000000.tar -', True),
 ]
 
-aggregators = [
-    Mean()
-]
-
-evaluate_retrieval(aggregators, eval_data, multicaption=True)
+evaluate_datasets_and_ckpts(eval_data)
